debb01668310a9bab31e25cfbd38b55db9aabc5d
Dakmeme <153366515+Dakmeme@users.noreply.github.com>
2025-07-22 14:04:57 +0700

    Update script.js

--- Đắc-Minh/script.js
+++ Đắc-Minh/script.js
@@ -1,22 +1,61 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyC-GR0nEyj6VFudhwJpISxicits-uLBJGw',
+  authDomain: 'ttcm-3ea96.firebaseapp.com',
+  projectId: 'ttcm-3ea96',
+  storageBucket: 'ttcm-3ea96.firebasestorage.app',
+  messagingSenderId: '196110779018',
+  appId: '1:196110779018:web:1d51451aaccbd244f2a373',
+}
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCi2NKH7Dzf6sLZdvuCQW18hxbsF4cVYB0",
-    authDomain: "ttmindx.firebaseapp.com",
-    projectId: "ttmindx",
-    storageBucket: "ttmindx.firebasestorage.app",
-    messagingSenderId: "499689288083",
-    appId: "1:499689288083:web:394be22db426aa48b93866",
-    measurementId: "G-Y0NCNLB337"
-  };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  getDocs,
+} from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js'
+
+const db = getFirestore(app)
+
+const form = document.getElementById('form')
+
+form.addEventListener('submit', async function (event) {
+  event.preventDefault()
+  console.log('Form submitted')
+
+  const contentValue = document.getElementById('content').value
+
+  try {
+    const docRef = await addDoc(collection(db, 'posts'), {
+      content: contentValue,
+      date: new Date().toISOString(),
+    })
+    console.log('Document written with ID: ', docRef.id)
+  } catch (e) {
+    console.error('Error adding document: ', e)
+  }
+})
+const posts = document.getElementById('posts')
+posts.innerHTML = ''
+const querySnapshot = await getDocs(collection(db, 'posts'))
+querySnapshot.forEach((doc) => {
+  console.log(doc.data())
+  const tr = document.createElement('tr')
+
+  tr.innerHTML = `
+    <td scope="col">${doc.id}</td>
+    <td scope="col">${doc.data().content}</td>
+    <td scope="col">${doc.data().date}</td>
+    <td scope="col">Action</td>
+    `
+
+  posts.appendChild(tr)
+})
